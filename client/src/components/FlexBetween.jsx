b549eab6f973bbce69c876c0a8525654eadba36b
Saurav Suman <sauravsuman5980@gmail.com>
2023-04-02 17:07:23 +0530

    navbar complete with dummy full Name

---
+++ client/src/components/FlexBetween.jsx
@@ -0,0 +1,10 @@
+import { Box } from "@mui/material";
+import { styled } from "@mui/system";
+
+const FLexBetween = styled(Box)({
+  display: "flex",
+  justifyContent: "space-between",
+  alignItems: "center",
+});
+
+export default FLexBetween;
