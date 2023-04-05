a0d0f5c68f7feeff2ab95d684474e14058d43d9a
Saurav Suman <sauravsuman5980@gmail.com>
2023-04-06 02:24:31 +0530

    navbar name

--- client/src/scenes/navbar/index.jsx
+++ client/src/scenes/navbar/index.jsx
@@ -56,7 +56,7 @@ const Navbar = () => {
             },
           }}
         >
-          Sociopedia
+          Mediafy
         </Typography>
         {isNonMobileScreens && (
           <FlexBetween
