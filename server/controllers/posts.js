3d2999a80cd9191d86b3246b7aaf32ec22471eff
Saurav Suman <sauravsuman5980@gmail.com>
2023-04-06 22:57:54 +0530

    Updated post.js sorted posts

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -27,7 +27,7 @@ export const createPost = async (req, res) => {
 // Read
 export const getFeedPosts = async (req, res) => {
   try {
-    const post = await Post.find();
+    const post = await Post.find().sort({ createdAt: -1 });
     res.status(200).json(post);
   } catch (err) {
     res.status(404).json({ message: err.message });
@@ -37,7 +37,7 @@ export const getFeedPosts = async (req, res) => {
 export const getUserPosts = async (req, res) => {
   try {
     const { userId } = req.params;
-    const post = await Post.find({ userId: userId });
+    const post = await Post.find({ userId: userId }).sort({ createdAt: -1 });
     res.status(200).json(post);
   } catch (err) {
     res.status(404).json({ message: err.message });
